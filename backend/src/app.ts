83a7ebe3c475ee4a05d47c690696688d99fc0472
kaido0427 <kounoumarcus@gmail.com>
2026-01-24 18:30:14 +0100

    db conect done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import authRoutes from './routes/auth.routes.js'
 
 const app = new Hono()
 
@@ -8,15 +9,17 @@ app.use('/*', cors())
 
 // Routes
 app.get('/', (c) => {
-  return c.json({ 
-    message: 'StaffTrack API',
-    version: '1.0.0'
-  })
+    return c.json({
+        message: 'StaffTrack API',
+        version: '1.0.0'
+    })
 })
 
 // Health check
 app.get('/health', (c) => {
-  return c.json({ status: 'ok' })
+    return c.json({ status: 'ok' })
 })
+
+app.route('/auth', authRoutes)
 
 export default app
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
+import 'dotenv/config'; 
 import { serve } from '@hono/node-server'
 import app from './app.js'
+import { connectDB } from "./utils/db.js";
 
-const port = 3000
+// Vérifiez que JWT_SECRET est chargé
+console.log('JWT_SECRET chargé:', process.env.JWT_SECRET ? '✅ OUI' : '❌ NON');
+
+await connectDB();
 
 serve({
+
+
   fetch: app.fetch,
-  port: port
+  port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
